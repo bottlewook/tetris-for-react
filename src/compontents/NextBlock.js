c30c0a000d506a1d8ea402aef45a03448c874fda
임병욱 <imbyeong-ug@imbyeong-ug-ui-MacBookPro.local>
2022-08-25 20:27:08 +0900

    Added connection for grid board

--- src/compontents/NextBlock.js
+++ src/compontents/NextBlock.js
@@ -1,12 +1,10 @@
 import GridSqure from "./GridSquare";
+import { useSelector } from "react-redux";
+import { shapes } from "../utils/index";
 
 const NextBlock = () => {
-  const box = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-  ];
+  const nextShape = useSelector((state) => state.game.nextShape);
+  const box = shapes[nextShape][0];
 
   const grid = box.map((rowArray, row) => {
     return rowArray.map((square, col) => {
