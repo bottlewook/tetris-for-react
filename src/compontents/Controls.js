362282d362455de2c20f37d505304ca5c26cea29
임병욱 <imbyeong-ug@imbyeong-ug-ui-MacBookPro.local>
2022-08-26 15:07:16 +0900

    Added connection for controls

--- src/compontents/Controls.js
+++ src/compontents/Controls.js
@@ -1,23 +1,48 @@
+import { useSelector, useDispatch } from "react-redux";
+import { moveDown, moveLeft, moveRight, rotate } from "../actions";
+
 const Controls = () => {
+  const dispatch = useDispatch();
+  const isRunning = useSelector((state) => state.isRunning);
   return (
     <div className="controls">
       {/* left */}
-      <button className="control-button" onClick={(e) => {}}>
+      <button
+        className="control-button"
+        onClick={(e) => {
+          dispatch(moveLeft());
+        }}
+      >
         Left
       </button>
 
       {/* right */}
-      <button className="control-button" onClick={(e) => {}}>
+      <button
+        className="control-button"
+        onClick={(e) => {
+          dispatch(moveRight());
+        }}
+      >
         Right
       </button>
 
       {/* rotate */}
-      <button className="control-button" onClick={(e) => {}}>
+      <button
+        className="control-button"
+        onClick={(e) => {
+          dispatch(rotate());
+        }}
+      >
         Rotate
       </button>
 
       {/* down */}
-      <button className="control-button" onClick={(e) => {}}>
+      <button
+        className="control-button"
+        onClick={(e) => {
+          dispatch(moveDown());
+        }}
+      >
         Down
       </button>
     </div>
--- src/compontents/GridBoard.js
+++ src/compontents/GridBoard.js
@@ -22,16 +22,17 @@ export default function GridBoard(props) {
   const { grid, shape, rotation, x, y, isRunning, speed } = game;
 
   const block = shapes[shape][rotation];
-  const blockColor = shape;
+  const blockColor = shape; //숫자
   // map rows
   const gridSquares = grid.map((rowArray, row) => {
     // map columns
     return rowArray.map((square, col) => {
       // Find the block x and y on the shape grid
       // By subtracting the x and y from the col and the row we get the position of the upper left corner of the block array as if it was superimposed over the main grid
-      const blockX = col - x;
-      const blockY = row - y;
+      const blockX = col - x; // 0 ~ 4
+      const blockY = row - y; // 4 ~ 21
       let color = square;
+
       // Map current falling block to grid.
       // For any squares that fall on the grid we need to look at the block array and see if there is a 1 in this case we use the block color.
       if (
